8555732003232fccc42f9c736c58a81b7a2eebff
alekspanteli <56685380+alekspanteli@users.noreply.github.com>
2022-09-14 16:20:14 +0300

    2 files modified, 1 added

---
+++ remove.js
@@ -0,0 +1,6 @@
+const nums = [100, 200, 300, 400, 500];
+
+document.write(nums.push(600))
+
+// let lastItem = nums.shift();
+// document.write(lastItem)
--- script.js
+++ script.js
@@ -1,7 +1,39 @@
 // Array Intro
 
+// const planets = [
+//   "Mars", 
+//   "Earth", 
+//   "Venus"
+// ]
+
+// console.log(planets[2])
+
+
+// Add elements
+
 const planets = [
   "Mars", 
   "Earth", 
   "Venus"
 ]
+
+// // we can add more than one item to an array
+
+// planets.push('test', 'test2')
+
+// // Add instruments to the beggining of an array
+
+// planets.unshift('test', 'test2')
+
+// Remove items from an array
+
+// planets.pop() //Last
+
+// planets.shift() First
+
+
+// get first or last item from an array
+
+// let lastItem = planets.push();
+
+// document.write(lastItem)
